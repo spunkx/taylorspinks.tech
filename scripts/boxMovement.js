ccf7936142d272d7024085f718925842b3539ee5
spunkx <spunkx+tspinks@our.ecu.edu.au>
2020-04-07 01:54:33 +0800

    Update boxMovement.js

--- scripts/boxMovement.js
+++ scripts/boxMovement.js
@@ -22,13 +22,15 @@ downbtn.addEventListener('click', (e) => {
     boxSelector.style.top = (parseFloat(boxSelector.style.top.replace("px", ""))+movementSpeed) + "px";
 })
 
-
+/*
 var map = {}
 onkeydown = onkeyup = function(e){
     //map idea from https://stackoverflow.com/questions/5203407/how-to-detect-if-multiple-keys-are-pressed-at-once-using-javascript
     //pushdown left-> pushdown shift results in being stuck in the if statement from line 33 to line 38
     e = e || event;
     map[e.keyCode] = e.type == 'keydown';
+    console.log("e.keyCode", e.keyCode)
+    console.log("e.type", e.type)
     console.log(map)
     if(e.keyCode == 16 && e.type == 'keydown'){
         movementSpeed = 5;
@@ -57,7 +59,97 @@ onkeydown = onkeyup = function(e){
         console.log("on down key " + movementSpeed)
         boxSelector.style.top = (parseFloat(boxSelector.style.top.replace("px", ""))+movementSpeed) + "px";
     }
+}*/
+
+let keyDown = document.addEventListener('keydown', (logKey) => {
+    if(logKey.keyCode == 37){
+        console.log(logKey.keyCode);
+        boxSelector.style.left = (parseFloat(boxSelector.style.left.replace("px", ""))-movementSpeed) + "px";
+    }
+    else if(logKey.keyCode == 39){
+        console.log(logKey.keyCode);
+        boxSelector.style.left = (parseFloat(boxSelector.style.left.replace("px", ""))+movementSpeed) + "px";
+    }
+    else if(logKey.keyCode == 38){
+        console.log(logKey.keyCode);
+        boxSelector.style.top = (parseFloat(boxSelector.style.top.replace("px", ""))-movementSpeed) + "px";
+    }
+    else if(logKey.keyCode == 40){
+        console.log(logKey.keyCode);
+        boxSelector.style.top = (parseFloat(boxSelector.style.top.replace("px", ""))+movementSpeed) + "px";
+    }
+
+})
+
+let keyUp = document.addEventListener('keyup', (logKey) => {
+    //keyDown;
+    console.log("key up lol");
+})
+
+
+//me learning recursion
+let i = 2;
+let n = 2;
+
+function isI(i,n){
+    isKill = false;
+    i = i*(-10);
+    console.log("i is currently!", i)
+    if (n == 1){
+        return i;
+    }
+    else{
+        return isI(i,n-1);
+    }
+}
+console.log("value of recursive calls", isI(i,n));
+
+
+//towers of hanoi?
+
+const diskNum = 3; //number of moves is 7 because 
+let startRadius = 1;
+var disks = [];
+let towerA = [];
+let towerB = [];
+let towerC = [];
+
+//write some unit tests please me
+
+function getDisks(diskNum, startRadius, disks){
+    currRadius = startRadius;
+    disks.push(currRadius);
+    if(diskNum === 1){
+        return disks;
+    }
+    else{
+        let nextRadius = currRadius+1; //moved this statement outside of return for readability
+        return getDisks(diskNum-1, nextRadius, disks);
+    }
+    
 }
 
-//create a key combination arrow function
+towerA = getDisks(diskNum, startRadius, disks);
+//towerA.forEach(ele => console.log(ele));
+let num = towerA.length;
+//stack storage for recording moves, push and pop (shift is correct in JavaScript)
+//fix up incorrect order for tower of hanoi
+function sortTower(towerA, towerC, towerB, n, numMoves){
+    //https://en.wikipedia.org/wiki/Tower_of_Hanoi
+    if(n > 0){
+        sortTower(towerA, towerB, towerC, n-1);
+        var temp = towerA.shift();
+        towerC.push(temp);
+        console.log(towerA, towerB, towerC);
+        sortTower(towerB, towerC, towerA, n-1);
+    }
+    return towerC;
+}
+
+console.log(sortTower(towerA, towerB, towerC, diskNum, 0));
+
+
+
+
+
 
